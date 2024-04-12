00f4c1c3cf0b636a1565641c7f0ccfa90f3f97bd
Thales Aguiar <dev.thalesaguiar@gmail.com>
2024-04-11 21:57:24 -0300

    feat: implement OnSignal push notification

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { OneSignal } from "react-native-onesignal";
+import { Routes } from "./src/routes";
 
-import { Routes } from './src/routes';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { CartContextProvider } from "./src/contexts/CartContext";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+OneSignal.initialize("ef772a7b-ad1e-4d47-90a4-278e44b6aaaa");
+OneSignal.Notifications.requestPermission(true);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
